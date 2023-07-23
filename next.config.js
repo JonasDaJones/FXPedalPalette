84df2bd7211928d7b7f1f1c8d3981a8a656590f2
JonasDaJones <johnny.jonas@gmx.net>
2023-07-23 11:59:42 +0200

    continue setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    compiler: {
+        styledComponents: true,
+    },
+    reactStrictMode: true,
+    images: {
+        domains: ["res.cloudinary.com"],
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+        });
 
-module.exports = nextConfig
+        return config;
+    },
+};
+
+module.exports = nextConfig;
